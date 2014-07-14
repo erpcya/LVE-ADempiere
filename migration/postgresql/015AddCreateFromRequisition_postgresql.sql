2a91ee7147e88782e870069a8d2d8b7deb9a673f
Carlos Parada <carlosaparadam@gmail.com>
2014-07-14 17:12:08 -0530

    Alter Table Add Column CreateFromRequisition

--- migration/postgresql/015AddCreateFromRequisition_postgresql.sql
+++ migration/postgresql/015AddCreateFromRequisition_postgresql.sql
@@ -833,3 +833,8 @@ INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,
 INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=74036 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
 ;
 
+-- 14/07/2014 06:09:22 PM VET
+-- Request for Quotation Create From Requisition
+UPDATE AD_Process SET AD_Browse_ID=50035,Updated=TO_TIMESTAMP('2014-07-14 18:09:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_ID=53654
+;
+
