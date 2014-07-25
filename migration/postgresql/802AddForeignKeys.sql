7f05e822e8e2bb4546526733f6d1f869b0d79134
selem <selem@localhost>
2014-07-25 17:03:49 +0200

    Add Warehouse Product Window

---
+++ migration/postgresql/802AddForeignKeys.sql
@@ -0,0 +1,19 @@
+﻿ALTER TABLE LVE_WarehouseProduct ADD CONSTRAINT FK_LVE_WarehouseProduct_AD_Table FOREIGN KEY (AD_Table_ID)
+	REFERENCES AD_Table(AD_Table_ID) 
+	ON UPDATE CASCADE 
+	ON DELETE RESTRICT;
+	
+ALTER TABLE LVE_WarehouseProductLine ADD CONSTRAINT FK_LVE_WarehouseProductLine_LVE_WarehouseProduct_ID FOREIGN KEY (LVE_WarehouseProduct_ID)
+	REFERENCES LVE_WarehouseProduct(LVE_WarehouseProduct_ID) 
+	ON UPDATE CASCADE 
+	ON DELETE RESTRICT;
+
+ALTER TABLE LVE_WarehouseProductLine ADD CONSTRAINT FK_LVE_WarehouseProductLine_M_Warehouse_ID FOREIGN KEY (M_Warehouse_ID)
+	REFERENCES M_Warehouse(M_Warehouse_ID) 
+	ON UPDATE CASCADE 
+	ON DELETE RESTRICT;
+	
+ALTER TABLE LVE_WarehouseProductLine ADD CONSTRAINT FK_LVE_WarehouseProductLine_M_Product_ID FOREIGN KEY (M_Product_ID)
+	REFERENCES M_Product(M_Product_ID) 
+	ON UPDATE CASCADE 
+	ON DELETE RESTRICT;
