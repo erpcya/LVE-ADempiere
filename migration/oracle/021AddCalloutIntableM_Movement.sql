66211dfae638126aaa9e846038a92ac9c4a554ec
Dixon Martinez <dixon.22martinez@gmail.com>
2014-09-25 09:14:42 -0530

    Add Callout In table M_Movement

---
+++ migration/oracle/021AddCalloutIntableM_Movement.sql
@@ -0,0 +1,5 @@
+-- Sep 25, 2014 10:10:51 AM VET
+-- LVE ADempiere
+UPDATE AD_Column SET Callout='org.spin.model.CalloutMovement.doctype',Updated=TO_DATE('2014-09-25 10:10:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=12201
+;
+
