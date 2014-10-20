2f319ddf67f41b2388ef20d25edb56baa2270e71
Dixon Martinez <dixon.22martinez@gmail.com>
2014-10-20 15:59:35 -0530

    Update project add scripts of project Custom-Import

---
+++ migration/oracle/033_AddPriceListFormatImport_oracle.sql
@@ -0,0 +1,40 @@
+-- Sep 20, 2013 5:08:21 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat (AD_Client_ID,AD_ImpFormat_ID,AD_Org_ID,AD_Table_ID,Created,CreatedBy,FormatType,IsActive,Name,Processing,SeparatorChar,Updated,UpdatedBy) VALUES (0,50022,0,53173,TO_DATE('2013-09-20 05:08:19','YYYY-MM-DD HH24:MI:SS'),100,'C','Y','Import PriceList','N',',',TO_DATE('2013-09-20 05:08:19','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:08:41 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56957,50022,50382,0,TO_DATE('2013-09-20 05:08:40','YYYY-MM-DD HH24:MI:SS'),100,'S','.','N',0,'Y','Name',10,1,TO_DATE('2013-09-20 05:08:40','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:09:08 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56968,50022,50383,0,TO_DATE('2013-09-20 05:09:06','YYYY-MM-DD HH24:MI:SS'),100,'S','.','N',0,'Y','SO PriceList',20,2,TO_DATE('2013-09-20 05:09:06','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:09:37 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataFormat,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56960,50022,50384,0,TO_DATE('2013-09-20 05:09:36','YYYY-MM-DD HH24:MI:SS'),100,'dd/MM/yyyy','D','.','N',0,'Y','Valid From',30,3,TO_DATE('2013-09-20 05:09:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:10:08 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56967,50022,50385,0,TO_DATE('2013-09-20 05:10:07','YYYY-MM-DD HH24:MI:SS'),100,'S','.','N',0,'Y','Product',40,4,TO_DATE('2013-09-20 05:10:07','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:10:34 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56962,50022,50386,0,TO_DATE('2013-09-20 05:10:34','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','PriceList',50,5,TO_DATE('2013-09-20 05:10:34','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:10:57 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56964,50022,50387,0,TO_DATE('2013-09-20 05:10:56','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','PriceStd',60,6,TO_DATE('2013-09-20 05:10:56','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Sep 20, 2013 5:11:17 AM VET
+-- LVE-Import
+INSERT INTO AD_ImpFormat_Row (AD_Client_ID,AD_Column_ID,AD_ImpFormat_ID,AD_ImpFormat_Row_ID,AD_Org_ID,Created,CreatedBy,DataType,DecimalPoint,DivideBy100,EndNo,IsActive,Name,SeqNo,StartNo,Updated,UpdatedBy) VALUES (0,56961,50022,50388,0,TO_DATE('2013-09-20 05:11:17','YYYY-MM-DD HH24:MI:SS'),100,'N','.','N',0,'Y','PriceLimit',70,7,TO_DATE('2013-09-20 05:11:17','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
