c433807f65de9a43da5678228a808c5d1d85c5d0
Dixon Martinez <dixon.22martinez@gmail.com>
2014-10-31 15:55:31 -0530

    Add scripts move project Withholding

---
+++ migration/postgresql/048UpdateWindowCashJournalLine.sql
@@ -0,0 +1,75 @@
+-- Oct 27, 2014 3:25:39 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_TIMESTAMP('2014-10-27 15:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74671
+;
+
+-- Oct 27, 2014 3:25:39 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_TIMESTAMP('2014-10-27 15:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68273
+;
+
+-- Oct 27, 2014 3:25:39 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_TIMESTAMP('2014-10-27 15:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68270
+;
+
+-- Oct 27, 2014 3:25:39 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=160,Updated=TO_TIMESTAMP('2014-10-27 15:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4197
+;
+
+-- Oct 27, 2014 3:25:39 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=170,Updated=TO_TIMESTAMP('2014-10-27 15:25:39','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74670
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=180,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4193
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=190,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68275
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=200,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5119
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=210,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5120
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=220,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68267
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=230,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68278
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=240,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68279
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=250,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68274
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=260,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
+-- Oct 27, 2014 3:25:40 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=270,Updated=TO_TIMESTAMP('2014-10-27 15:25:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68269
+;
+
---
+++ migration/postgresql/050UpdateLogicVisualizationCashJournalLine.sql
@@ -0,0 +1,265 @@
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=40,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4198
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=50,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74670
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=70,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4199
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=80,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68277
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=90,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68280
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=100,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4250
+;
+
+-- Oct 31, 2014 4:25:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=110,Updated=TO_TIMESTAMP('2014-10-31 16:25:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:25:15 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=40,Updated=TO_TIMESTAMP('2014-10-31 16:25:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74670
+;
+
+-- Oct 31, 2014 4:25:16 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=50,Updated=TO_TIMESTAMP('2014-10-31 16:25:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4198
+;
+
+-- Oct 31, 2014 4:25:56 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:25:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74670
+;
+
+-- Oct 31, 2014 4:26:00 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:26:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4201
+;
+
+-- Oct 31, 2014 4:26:07 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET AD_Reference_ID=14,Updated=TO_TIMESTAMP('2014-10-31 16:26:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4199
+;
+
+-- Oct 31, 2014 4:26:48 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:26:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68277
+;
+
+-- Oct 31, 2014 4:26:52 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:26:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68280
+;
+
+-- Oct 31, 2014 4:27:01 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@=I',Updated=TO_TIMESTAMP('2014-10-31 16:27:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:27:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_TIMESTAMP('2014-10-31 16:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4197
+;
+
+-- Oct 31, 2014 4:27:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_TIMESTAMP('2014-10-31 16:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4194
+;
+
+-- Oct 31, 2014 4:27:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_TIMESTAMP('2014-10-31 16:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74671
+;
+
+-- Oct 31, 2014 4:27:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=160,Updated=TO_TIMESTAMP('2014-10-31 16:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68273
+;
+
+-- Oct 31, 2014 4:27:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=170,Updated=TO_TIMESTAMP('2014-10-31 16:27:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68270
+;
+
+-- Oct 31, 2014 4:27:53 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:27:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4197
+;
+
+-- Oct 31, 2014 4:28:04 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=100,Updated=TO_TIMESTAMP('2014-10-31 16:28:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74671
+;
+
+-- Oct 31, 2014 4:28:04 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=110,Updated=TO_TIMESTAMP('2014-10-31 16:28:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4250
+;
+
+-- Oct 31, 2014 4:28:04 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_TIMESTAMP('2014-10-31 16:28:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:28:04 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_TIMESTAMP('2014-10-31 16:28:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68271
+;
+
+-- Oct 31, 2014 4:28:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_TIMESTAMP('2014-10-31 16:28:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4197
+;
+
+-- Oct 31, 2014 4:28:05 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_TIMESTAMP('2014-10-31 16:28:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4194
+;
+
+-- Oct 31, 2014 4:28:13 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:28:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4250
+;
+
+-- Oct 31, 2014 4:28:18 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='N',Updated=TO_TIMESTAMP('2014-10-31 16:28:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:28:30 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:28:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68270
+;
+
+-- Oct 31, 2014 4:28:42 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:28:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68279
+;
+
+-- Oct 31, 2014 4:28:45 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:28:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68274
+;
+
+-- Oct 31, 2014 4:28:48 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:28:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68274
+;
+
+-- Oct 31, 2014 4:28:57 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = I',Updated=TO_TIMESTAMP('2014-10-31 16:28:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
+-- Oct 31, 2014 4:29:00 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = I',Updated=TO_TIMESTAMP('2014-10-31 16:29:00','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68269
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=120,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68271
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=130,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4197
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=140,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4194
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=150,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68273
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=160,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68270
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=170,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4193
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=180,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68275
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=190,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5119
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=200,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=5120
+;
+
+-- Oct 31, 2014 4:30:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=210,Updated=TO_TIMESTAMP('2014-10-31 16:30:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68267
+;
+
+-- Oct 31, 2014 4:30:07 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=220,Updated=TO_TIMESTAMP('2014-10-31 16:30:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:30:13 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:30:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68268
+;
+
+-- Oct 31, 2014 4:31:18 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = I & @TenderType@ ! X',Updated=TO_TIMESTAMP('2014-10-31 16:31:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68269
+;
+
+-- Oct 31, 2014 4:31:54 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=260,Updated=TO_TIMESTAMP('2014-10-31 16:31:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68269
+;
+
+-- Oct 31, 2014 4:31:54 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=270,Updated=TO_TIMESTAMP('2014-10-31 16:31:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
+-- Oct 31, 2014 4:31:58 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_TIMESTAMP('2014-10-31 16:31:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
+-- Oct 31, 2014 4:32:14 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = I & @TenderType@ = C',Updated=TO_TIMESTAMP('2014-10-31 16:32:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
+-- Oct 31, 2014 4:33:38 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET DisplayLogic='@CashType@ = I & @TenderType@ = K',Updated=TO_TIMESTAMP('2014-10-31 16:33:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=68272
+;
+
