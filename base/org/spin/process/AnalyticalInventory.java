5a57711fd0cd4ed913a6cc5abb2cf6cab252c5f2
Yamel Senih <yamelsenih@gmail.com>
2014-11-25 04:24:57 +0100

    Add Support to Summary Balance Product on report Analytical Inventory

--- base/org/spin/process/AnalyticalInventory.java
+++ base/org/spin/process/AnalyticalInventory.java
@@ -122,16 +122,23 @@ public class AnalyticalInventory extends SvrProcess {
 				"		WHEN t.MovementQty > 0 THEN t.MovementQty " +
 				"		ELSE 0 " +
 				"	END" +
-				", 0)) QtyIn, " +
-				"0 CurrentCostPrice, " + 
-				"SUM(" +
+				", 0)) QtyIn, ");
+		//	Add Current Cost Price
+		if(p_MovementDate_To != null)
+			sql.append("0 CurrentCostPrice, ");
+		else
+			sql.append("productCostPriceAt(p.M_Product_ID, ?) CurrentCostPrice, ");
+		//	
+		sql.append("SUM(" +
 				"	t.MovementQty * productCostPriceAt(t.M_Product_ID, t.MovementDate)" + 
 				") CumulatedAmt, " + 
 				//	Date Movement
-				"? MovementDate, " +
+				DB.TO_DATE(p_MovementDate, true) + " MovementDate, " +
 				"'PB' MovementType, " +
 				"0 Multiply, " +
-				"'" + Msg.translate(getCtx(), "PreviousBalance") + "' DocumentNo, " +
+				"'" + Msg.translate(getCtx(), (p_MovementDate_To != null
+													? "PreviousBalance"
+															: "Balance")) + "' DocumentNo, " +
 				"0 seqNo, " +
 				"t.AD_Client_ID, " +
 				"t.AD_Org_ID, " +
@@ -143,88 +150,114 @@ public class AnalyticalInventory extends SvrProcess {
 				"INNER JOIN M_Locator l ON(l.M_Locator_ID = t.M_Locator_ID) " +
 				"INNER JOIN M_Warehouse w ON(w.M_Warehouse_ID = l.M_Warehouse_ID) " +
 				//	Where
-				"WHERE t.AD_Client_ID = " + getAD_Client_ID() + " " + 
-				"AND t.MovementDate < ? ");
-				//	Clause Where Add
+				"WHERE t.AD_Client_ID = " + getAD_Client_ID() + " ");
+		//	Add Movement Date
+		if(p_MovementDate_To != null)
+			sql.append("AND t.MovementDate < ? ");
+		else
+			sql.append("AND t.MovementDate <= ? ");
+		//	Clause Where Add
 		sql.append(m_optionalWhere);
-				//	Group By
-		sql.append("GROUP BY " +
-				"w.M_Warehouse_ID, " +
-				"w.Name, " +
-				"l.M_Locator_ID, " +
-				"l.Value, " +
-				"c.M_Product_Category_ID, " +
-				"c.Value, " +
-				"c.Name, " +
-				"p.M_Product_ID, " +
-				"p.Value, " +
-				"p.Name, " +
-				"p.C_UOM_ID, " +
-				"t.AD_Client_ID, " +
-				"t.AD_Org_ID ");
+		//	Group By
+		if(p_MovementDate_To != null) {
+			sql.append("GROUP BY " +
+					"t.AD_Org_ID, " +
+					"w.M_Warehouse_ID, " +
+					"w.Name, " +
+					"l.M_Locator_ID, " +
+					"l.Value, " +
+					"c.M_Product_Category_ID, " +
+					"c.Value, " +
+					"c.Name, " +
+					"p.M_Product_ID, " +
+					"p.Value, " +
+					"p.Name, " +
+					"p.C_UOM_ID, " +
+					"t.AD_Client_ID ");
+		} else {
+			sql.append("GROUP BY " +
+					"t.AD_Client_ID, " +
+					"c.M_Product_Category_ID, " +
+					"c.Value, " +
+					"c.Name, " +
+					"p.M_Product_ID, " +
+					"p.Value, " +
+					"p.Name, " +
+					"p.C_UOM_ID, " +
+					"t.AD_Org_ID, " +
+					"w.M_Warehouse_ID, " +
+					"w.Name, " +
+					"l.M_Locator_ID, " +
+					"l.Value ");
+		}
 		//	Union
-		sql.append("UNION ALL ");
-		//	Transaction
-		sql.append("SELECT " +
-				"w.M_Warehouse_ID, " +
-				"w.Name Warehouse, " +
-				"l.M_Locator_ID, " +
-				"l.Value Locator, " +
-				"c.M_Product_Category_ID, " +
-				"(c.Value || '-' || c.Name) Category, " +
-				"p.M_Product_ID, " +
-				"(p.Value || '-' || p.Name) Product, " +
-				"p.C_UOM_ID, " +
-				//	Quantity Out
-				"coalesce(CASE " +
-				"	WHEN t.MovementQty < 0 THEN Abs(t.MovementQty) " +
-				"	ELSE 0 " +
-				"END, 0) " +
-				"QtyOut, " +
-				//	Quantity In
-				"coalesce(CASE " +
-				"	WHEN t.MovementQty > 0 THEN t.MovementQty " +
-				"	ELSE 0 " +
-				"END, 0) " +
-				"QtyIn, " +
-				"productCostPriceAt(t.M_Product_ID, t.MovementDate) CurrentCostPrice, " + 
-				"0 CumulatedAmt, " + 
-				"t.MovementDate, " +
-				"t.MovementType, " +
-				"1 Multiply, " +
-				//	Document No
-				"CASE " +
-				"	WHEN mio.DocumentNo IS NOT NULL THEN mio.DocumentNo " +
-				"	WHEN mi.DocumentNo IS NOT NULL THEN mi.DocumentNo " +
-				"	WHEN mo.DocumentNo IS NOT NULL THEN mo.DocumentNo " +
-				"	ELSE mppp.Name " +
-				"END " +
-				"DocumentNo, " +
-				"1 SeqNo, " +
-				"t.AD_Client_ID, " +
-				"t.AD_Org_ID, " +
-				getAD_PInstance_ID() + " AD_PInstance_ID " +
-				//	From
-				"FROM M_Transaction t " +
-				"INNER JOIN M_Product p ON(t.M_Product_ID = p.M_Product_ID) " +
-				"INNER JOIN M_Product_Category c ON(c.M_Product_Category_ID = p.M_Product_Category_ID) " +
-				"INNER JOIN M_Locator l ON(l.M_Locator_ID = t.M_Locator_ID) " +
-				"INNER JOIN M_Warehouse w ON(w.M_Warehouse_ID = l.M_Warehouse_ID) " +
-				"LEFT JOIN M_InoutLine miol ON(miol.M_InoutLine_ID = t.M_InoutLine_ID) " +
-				"LEFT JOIN M_Inout mio ON(miol.M_Inout_ID = mio.M_Inout_ID) " +
-				"LEFT JOIN M_InventoryLine mil ON(mil.M_InventoryLine_ID = t.M_InventoryLine_ID) " +
-				"LEFT JOIN M_Inventory mi ON(mi.M_Inventory_ID = mil.M_Inventory_ID) " +
-				"LEFT JOIN M_MovementLine mol ON(mol.M_MovementLine_ID = t.M_MovementLine_ID) " +
-				"LEFT JOIN M_Movement mo ON(mo.M_Movement_ID = mol.M_Movement_ID) " +
-				"LEFT JOIN M_ProductionLine mpl ON(mpl.M_ProductionLine_ID = t.M_ProductionLine_ID) " +
-				"LEFT JOIN M_ProductionPlan mpp ON(mpp.M_ProductionPlan_ID = mpl.M_ProductionPlan_ID) " +
-				"LEFT JOIN M_Production mppp ON(mppp.M_Production_ID = mpp.M_Production_ID) " +
-				//	Where
-				"WHERE t.MovementDate BETWEEN ? AND ? ");
-				//	Clause Where Add
-		sql.append(m_optionalWhere);
-		//	Order By
-		sql.append("ORDER BY Warehouse, Locator, Category, Product, SeqNo, MovementDate");
+		if(p_MovementDate_To != null) {
+			sql.append("UNION ALL ");
+			//	Transaction
+			sql.append("SELECT " +
+					"w.M_Warehouse_ID, " +
+					"w.Name Warehouse, " +
+					"l.M_Locator_ID, " +
+					"l.Value Locator, " +
+					"c.M_Product_Category_ID, " +
+					"(c.Value || '-' || c.Name) Category, " +
+					"p.M_Product_ID, " +
+					"(p.Value || '-' || p.Name) Product, " +
+					"p.C_UOM_ID, " +
+					//	Quantity Out
+					"coalesce(CASE " +
+					"	WHEN t.MovementQty < 0 THEN Abs(t.MovementQty) " +
+					"	ELSE 0 " +
+					"END, 0) " +
+					"QtyOut, " +
+					//	Quantity In
+					"coalesce(CASE " +
+					"	WHEN t.MovementQty > 0 THEN t.MovementQty " +
+					"	ELSE 0 " +
+					"END, 0) " +
+					"QtyIn, " +
+					"productCostPriceAt(t.M_Product_ID, t.MovementDate) CurrentCostPrice, " + 
+					"0 CumulatedAmt, " + 
+					"t.MovementDate, " +
+					"t.MovementType, " +
+					"1 Multiply, " +
+					//	Document No
+					"CASE " +
+					"	WHEN mio.DocumentNo IS NOT NULL THEN mio.DocumentNo " +
+					"	WHEN mi.DocumentNo IS NOT NULL THEN mi.DocumentNo " +
+					"	WHEN mo.DocumentNo IS NOT NULL THEN mo.DocumentNo " +
+					"	ELSE mppp.Name " +
+					"END " +
+					"DocumentNo, " +
+					"1 SeqNo, " +
+					"t.AD_Client_ID, " +
+					"t.AD_Org_ID, " +
+					getAD_PInstance_ID() + " AD_PInstance_ID " +
+					//	From
+					"FROM M_Transaction t " +
+					"INNER JOIN M_Product p ON(t.M_Product_ID = p.M_Product_ID) " +
+					"INNER JOIN M_Product_Category c ON(c.M_Product_Category_ID = p.M_Product_Category_ID) " +
+					"INNER JOIN M_Locator l ON(l.M_Locator_ID = t.M_Locator_ID) " +
+					"INNER JOIN M_Warehouse w ON(w.M_Warehouse_ID = l.M_Warehouse_ID) " +
+					"LEFT JOIN M_InoutLine miol ON(miol.M_InoutLine_ID = t.M_InoutLine_ID) " +
+					"LEFT JOIN M_Inout mio ON(miol.M_Inout_ID = mio.M_Inout_ID) " +
+					"LEFT JOIN M_InventoryLine mil ON(mil.M_InventoryLine_ID = t.M_InventoryLine_ID) " +
+					"LEFT JOIN M_Inventory mi ON(mi.M_Inventory_ID = mil.M_Inventory_ID) " +
+					"LEFT JOIN M_MovementLine mol ON(mol.M_MovementLine_ID = t.M_MovementLine_ID) " +
+					"LEFT JOIN M_Movement mo ON(mo.M_Movement_ID = mol.M_Movement_ID) " +
+					"LEFT JOIN M_ProductionLine mpl ON(mpl.M_ProductionLine_ID = t.M_ProductionLine_ID) " +
+					"LEFT JOIN M_ProductionPlan mpp ON(mpp.M_ProductionPlan_ID = mpl.M_ProductionPlan_ID) " +
+					"LEFT JOIN M_Production mppp ON(mppp.M_Production_ID = mpp.M_Production_ID) " +
+					//	Where
+					"WHERE t.MovementDate BETWEEN ? AND ? ");
+					//	Clause Where Add
+			sql.append(m_optionalWhere);	
+			//	Order By
+			sql.append("ORDER BY Warehouse, Locator, Category, Product, SeqNo, MovementDate");
+		} else {
+			//	Order By
+			sql.append("ORDER BY Category, Product, Warehouse, Locator ");
+		}
 		//	
 		log.fine("SQL = " + sql.toString());
 		//	
@@ -237,10 +270,11 @@ public class AnalyticalInventory extends SvrProcess {
 			//	
 			pstmt = DB.prepareStatement (sql.toString(), null);
 			//	Query 1 
-			//	Movement Date Column
-			pstmt.setTimestamp(i++, p_MovementDate);
+			//	Movement Date for Cost
+			if(p_MovementDate_To == null)
+				pstmt.setTimestamp(i++, p_MovementDate);
 			//	Movement Date Where
-			pstmt.setTimestamp(i++, p_MovementDate);			
+			pstmt.setTimestamp(i++, p_MovementDate);
 			//	Optional Organization
 			if (p_AD_Org_ID != 0){
 				pstmt.setInt(i++, p_AD_Org_ID);
@@ -266,33 +300,35 @@ public class AnalyticalInventory extends SvrProcess {
 				pstmt.setString(i++, p_MovementType);
 			}
 			//	Query 2
-			//	Movement Date
-			pstmt.setTimestamp(i++, p_MovementDate);
-			//	Movement Date To
-			pstmt.setTimestamp(i++, p_MovementDate_To);			
-			//	Optional Organization
-			if (p_AD_Org_ID != 0){
-				pstmt.setInt(i++, p_AD_Org_ID);
-			}
-			//	Optional Warehouse
-			if (p_M_Warehouse_ID != 0){
-				pstmt.setInt(i++, p_M_Warehouse_ID);
-			}
-			//	Optional Locator
-			if (p_M_Locator_ID != 0){
-				pstmt.setInt(i++, p_M_Locator_ID);
-			}
-			//	Optional Product Category
-			if (p_M_Product_Category_ID != 0){
-				pstmt.setInt(i++, p_M_Product_Category_ID);
-			}
-			//	Optional Product
-			if (p_M_Product_ID != 0){
-				pstmt.setInt(i++, p_M_Product_ID);
-			}
-			//	Optional Movement
-			if (p_MovementType != null){
-				pstmt.setString(i++, p_MovementType);
+			if(p_MovementDate_To != null) {
+				//	Movement Date
+				pstmt.setTimestamp(i++, p_MovementDate);
+				//	Movement Date To
+				pstmt.setTimestamp(i++, p_MovementDate_To);			
+				//	Optional Organization
+				if (p_AD_Org_ID != 0){
+					pstmt.setInt(i++, p_AD_Org_ID);
+				}
+				//	Optional Warehouse
+				if (p_M_Warehouse_ID != 0){
+					pstmt.setInt(i++, p_M_Warehouse_ID);
+				}
+				//	Optional Locator
+				if (p_M_Locator_ID != 0){
+					pstmt.setInt(i++, p_M_Locator_ID);
+				}
+				//	Optional Product Category
+				if (p_M_Product_Category_ID != 0){
+					pstmt.setInt(i++, p_M_Product_Category_ID);
+				}
+				//	Optional Product
+				if (p_M_Product_ID != 0){
+					pstmt.setInt(i++, p_M_Product_ID);
+				}
+				//	Optional Movement
+				if (p_MovementType != null){
+					pstmt.setString(i++, p_MovementType);
+				}
 			}
 			//	
 			rs = pstmt.executeQuery();
