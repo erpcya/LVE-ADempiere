90e9506c7c57fee80b28a6ddf7bc0bbb8dfc0948
Dixon Martinez <dixon.22martinez@gmail.com>
2014-12-10 18:01:38 -0430

    Add document affected and update model validator for after complete invoice add allocation

---
+++ migration/oracle/060UpdateWindowInvoiceCustomer.sql
@@ -0,0 +1,85 @@
+-- Dec 9, 2014 2:58:24 PM VET
+-- LVE ADempiere
+DELETE  FROM  AD_Field_Trl WHERE AD_Field_ID=71645
+;
+
+-- Dec 9, 2014 2:58:24 PM VET
+-- LVE ADempiere
+DELETE FROM AD_Field WHERE AD_Field_ID=71645
+;
+
+-- Dec 9, 2014 2:58:38 PM VET
+-- LVE ADempiere
+DELETE  FROM  AD_Field_Trl WHERE AD_Field_ID=71651
+;
+
+-- Dec 9, 2014 2:58:38 PM VET
+-- LVE ADempiere
+DELETE FROM AD_Field WHERE AD_Field_ID=71651
+;
+
+-- Dec 9, 2014 2:58:57 PM VET
+-- LVE ADempiere
+DELETE  FROM  AD_Column_Trl WHERE AD_Column_ID=70655
+;
+
+-- Dec 9, 2014 2:58:57 PM VET
+-- LVE ADempiere
+DELETE FROM AD_Column WHERE AD_Column_ID=70655
+;
+
+-- Dec 9, 2014 2:59:23 PM VET
+-- LVE ADempiere
+UPDATE AD_Column SET AD_Val_Rule_ID=52280,Updated=TO_DATE('2014-12-09 14:59:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66490
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=180,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=67861
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=190,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=12746
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=200,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=13660
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=210,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=13661
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=220,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=13692
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=230,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=13666
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=240,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=13667
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=250,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2995
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=260,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=8243
+;
+
+-- Dec 9, 2014 10:12:03 PM VET
+-- LVE ADempiere
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=270,Updated=TO_DATE('2014-12-09 22:12:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=8265
+;
+
