2b35e0c1b722f125ebb2df54ad3872ca8936079f
Dixon Martinez <dixon.22martinez@gmail.com>
2015-01-13 10:56:03 -0430

    Add scripts for support user doc sequence

---
+++ migration/postgresql/067UpdateSmartBrowse.sql
@@ -0,0 +1,10 @@
+-- Dec 19, 2014 11:37:59 AM VET
+-- LVE ADempiere
+UPDATE AD_Val_Rule SET Code='C_Order.IsSoTrx=''Y'' AND DocStatus IN (''CO'',''IP'')',Updated=TO_TIMESTAMP('2014-12-19 11:37:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=52412
+;
+
+-- Dec 19, 2014 12:20:06 PM VET
+-- LVE ADempiere
+UPDATE AD_Val_Rule SET Code='C_Order.IsSoTrx=''Y'' AND DocStatus IN (''CO'',''IP'')',Updated=TO_DATE('2014-12-19 12:20:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=52412
+;
+
